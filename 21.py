126e1843b810eb11f2c75538f702e7b6efd10923
Fleetwood <flyoungiv@gmail.com>
2020-11-04 09:34:45 -0600

    remove unused function

--- 21.py
+++ 21.py
@@ -27,12 +27,6 @@ def divisor_sums_in_range(n):
     return number_pairs
 
 
-def is_amicable_pair(p1, p2):
-    if p1[0] == p2[1] and p1[1] == p2[0]:
-        return True
-    return False
-
-
 def has_amicable_pair(t):
     if t[0] == t[1]:  # ignore numbers like 6 or 28 where sum of proper divisors is itself
         return False
